6cdf799c3ce5cbcfb147b97f73714f2fc0293ab8
Julian-Kellmer <kellmer002@gmail.com>
2026-02-05 13:00:22 -0300

    fixed servicios pt

--- src/servicios/index.jsx
+++ src/servicios/index.jsx
@@ -3,7 +3,7 @@ import ServicesList from './sections/ServicesList'
 
 const Servicios = () => {
   return (
-    <main className='bg-background min-h-screen  md:pt-32 pb-32'>
+    <main className='bg-background min-h-screen pt-32 md:pt-32 pb-32'>
       <div className='layout-wrap'>
         <div className='layout-grid col-span-full mb-16  flex items-start '>
           <h1 className='col-span-full md:col-span-8 md:col-start-2 text-h1 leading-tight font-medium text-primary'>
